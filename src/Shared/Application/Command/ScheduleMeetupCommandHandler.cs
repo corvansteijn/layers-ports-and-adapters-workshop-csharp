aa8b44f8714f3d840b89187fe205ab71e7bd7662
Cor van Steijn <cor@vansteijn.info>
2017-04-07 16:54:14 +0200

    rabbitmq

--- src/Shared/Application/Command/ScheduleMeetupCommandHandler.cs
+++ src/Shared/Application/Command/ScheduleMeetupCommandHandler.cs
@@ -3,23 +3,26 @@ using Shared.Entity;
 
 namespace Shared.Application.Command
 {
+    public delegate void NotifyMeetupCreated(Guid id);
     public class ScheduleMeetupCommandHandler
     {
         private readonly IMeetupRepository meetupRepository;
+        private readonly NotifyMeetupCreated created;
 
-        public ScheduleMeetupCommandHandler(IMeetupRepository meetupRepository)
+        public ScheduleMeetupCommandHandler(IMeetupRepository meetupRepository, NotifyMeetupCreated created)
         {
             this.meetupRepository = meetupRepository;
+            this.created = created;
         }
 
         public void Handle(string name, string description, string scheduledFor)
         {
-            meetupRepository.Add(Meetup.Schedule(
             Guid id = Guid.NewGuid();
             meetupRepository.Add(Meetup.Schedule(id,
                 Name.FromString(name),
                 Description.FromString(description), 
-                DateTimeOffset.Parse(scheduledFor)));            
+                DateTimeOffset.Parse(scheduledFor)));
+            created(id);
         }
     }
 }
--- src/Shared/Infrastructure/Bootstrapper.cs
+++ src/Shared/Infrastructure/Bootstrapper.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
 using Shared.Application;
 using Shared.Application.Command;
 using Shared.Entity;
@@ -13,6 +15,24 @@ namespace Shared
         {
             services.AddTransient<IMeetupRepository>(p => new MeetupRepository("//app//var//meetup.txt"));
             services.AddTransient<ScheduleMeetupProvider>();
+            services.AddTransient<NotifyMeetupCreated>(p =>
+            {
+                return id =>
+                {
+                    ConnectionFactory factory = new ConnectionFactory();
+                    factory.Uri = "amqp://guest:guest@rabbitmq:5672";
+
+                    IConnection conn = factory.CreateConnection();
+                    IModel channel = conn.CreateModel();
+                    string exchangeName = "dsfds";
+                    channel.QueueDeclare("MyQueue", exclusive:false);
+
+                    byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes(id.ToString());
+                    channel.BasicPublish("", "MyQueue", null, messageBodyBytes);
+
+                    //conn.Close();
+                };
+            });
             services.AddTransient<MeetupApplicationConfig>();
             services.AddTransient<ScheduleMeetupCommandHandler>();
 
